9a8cb4a6fd91df655a3dd6cef7654d72dbb627e0
Selmane Lamia <hl_selmane@esi.dz>
2021-05-16 03:33:37 +0100

    button up done

--- app/src/main/java/com/example/doctors/MainActivity.kt
+++ app/src/main/java/com/example/doctors/MainActivity.kt
@@ -2,21 +2,21 @@ package com.example.doctors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val nav = findNavController(this,R.id.nav_host)
+        val nav = findNavController(R.id.nav_host_fragment)
         setupActionBarWithNavController(nav)
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
 
-        val navController = findNavController(this,R.id.nav_host)
+        val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp()
     }
 }
