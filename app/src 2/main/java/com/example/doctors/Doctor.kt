1f331065c922d0c383d0cfa31038d05d5dd5a7ac
Selmane Lamia <hl_selmane@esi.dz>
2021-05-16 03:10:17 +0100

    button up process

--- app/src 2/main/java/com/example/doctors/Doctor.kt
+++
@@ -1,8 +0,0 @@
-package com.example.doctors
-
-import java.io.Serializable
-
-data class Doctor(
-    val nom:String, val prenom:String, val num:String,
-    val speciality:String, val img:Int, val lat: Double, val lang: Double
-):Serializable
--- app/src/main/java/com/example/doctors/MainActivity.kt
+++ app/src/main/java/com/example/doctors/MainActivity.kt
@@ -2,17 +2,21 @@ package com.example.doctors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_main.*
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nav = findNavController(this,R.id.nav_host)
+        setupActionBarWithNavController(nav)
 
     }
 
+    override fun onSupportNavigateUp(): Boolean {
 
+        val navController = findNavController(this,R.id.nav_host)
+        return navController.navigateUp()
+    }
 }
